655f5bf3bb9b1535a6923acf4de2ca270a518009
derek S-C <derek.sternberg.costanzo@outlook.com>
2023-02-21 16:51:54 -0600

    aded Tim
    i added Tim

--- stablistaton/src/main/java/frc/robot/commands/yAxisStabilization.java
+++ stablistaton/src/main/java/frc/robot/commands/yAxisStabilization.java
@@ -1,32 +1,92 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
 
+import frc.robot.subsystems.Drivetrain;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class zAxisStabilization extends CommandBase {
-  /** Creates a new zAxisStabilization. */
-  public zAxisStabilization() {
-    // Use addRequirements() here to declare subsystem dependencies.
+public class yAxisStabilization extends CommandBase {
+  private final Drivetrain m_drive;
+  private final double m_distance;
+  private final double m_speed;
+  private final boolean m_stable;
+  private int m_Tim;
+  /**
+   * Creates a new DriveDistance. This command will drive your your robot for a desired distance at
+   * a desired speed.
+   *
+   * @param speed The speed at which the robot will drive
+   * @param inches The number of inches the robot will drive
+   * @param drive The drivetrain subsystem on which this command will run
+   */
+  public yAxisStabilization(double speed, double inches, Boolean stable, Integer Tim, Drivetrain drive) {
+    m_distance = inches;
+    m_speed = speed;
+    m_stable = stable;
+    m_Tim = Tim;
+    m_drive = drive;
+    addRequirements(drive);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_drive.arcadeDrive(0, 0);
+    m_drive.resetEncoders();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_drive.arcadeDrive(m_speed, 0);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_drive.arcadeDrive(0, 0);
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    // Compare distance travelled from start to desired distance
+    // return Math.abs(m_drive.getAverageDistanceInch()) >= m_distance;
+    return m_stable;
   }
 }
+// Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
+
+// package frc.robot.commands;
+
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+
+// public class zAxisStabilization extends CommandBase {
+//   /** Creates a new zAxisStabilization. */
+//   public zAxisStabilization() {
+//     // Use addRequirements() here to declare subsystem dependencies.
+//   }
+
+//   // Called when the command is initially scheduled.
+//   @Override
+//   public void initialize() {}
+
+//   // Called every time the scheduler runs while the command is scheduled.
+//   @Override
+//   public void execute() {}
+
+//   // Called once the command ends or is interrupted.
+//   @Override
+//   public void end(boolean interrupted) {}
+
+//   // Returns true when the command should end.
+//   @Override
+//   public boolean isFinished() {
+//     return false;
+//   }
+// }
